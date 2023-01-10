0ff68a16f88f39c67e6fc30afcb00be38ecaa0fb
khadiza21 <bibikhadiza474@gmail.com>
2023-01-10 21:41:24 +0600

    lightOJ-WarmUP-Greetings

---
+++ warm-UP/greetings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cases, num1, num2;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        cin >> num1 >> num2;
+        cout<<"Case "<<i+1<<": "<<num1+num2<<endl;
+    }
+
+return 0;
+}
