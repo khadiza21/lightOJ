46b5c356629a5441a16bcc921b374141a81aa818
khadiza21 <bibikhadiza474@gmail.com>
2023-01-11 21:13:19 +0600

    brush

---
+++ warm-UP/brush.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cases;
+    cin >> cases;
+    for (int i = 1; i <= cases; i++) {
+
+        int size=0, dust = 0;
+        cin>>size;
+        int studentNums[size];
+        for (int j = 1; j <= size; j++) {
+            cin >> studentNums[j];
+            if(studentNums[j]>0){
+               dust += studentNums[j]; 
+            }
+            
+        }
+        cout<< "Case "<< i << ": "<< dust<<endl;
+        
+    }
+    return 0;
+}
---
+++ warm-UP/fantablousBirthday.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cases, x, y, seconds;
+    cin >> cases;
+    for (int i = 0; i < cases; i++)
+    {
+        cin >> seconds;
+        cout << "case " << i + 1 << ": " << x << " " << y << endl;
+    }
+
+    return 0;
+}
