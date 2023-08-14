bbf367f36d573d46fc8759c3faf19e04533c2cb5
khadiza21 <bibikhadiza474@gmail.com>
2023-08-14 22:44:08 +0600

    1001 light oj

--- problem_slv/1001.cpp
+++ problem_slv/1001.cpp
@@ -1,8 +1,19 @@
-#include "iostream"
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-  cout << "hello world" << "\n";
-  return 0;
+int main() {
+  int cases,problems_num;
+  cin>>cases;
+  for(int i=0; i<cases; i++){
+      cin>>problems_num;
+      if(problems_num>10){
+         cout<<10<<" "<<problems_num-10<<endl;  
+      }else if (problems_num<10){
+         cout<<0<<" "<<problems_num<<endl;
+      }else{
+            cout<<10<<" "<<0<<endl;
+      }
+     
+  }
+    return 0;
 }
-
